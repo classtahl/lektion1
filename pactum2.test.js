9d27b9ec2586fb2e7083df8d53e0c97775282402
Clas Ståhl <clas.stahl@edu.ecutbildning.se>
2022-02-21 11:18:17 +0100

    new test to upload

---
+++ pactum2.test.js
@@ -0,0 +1,9 @@
+const pactum = require('pactum');
+
+it('Get 401 status with faulty user credentials, this means that login is incorrect', async () => {
+await pactum.spec()
+  .get('https://handla.api.ica.se/api/login/')
+  .withAuth('Authorization', 'Basic MTk3MzAyMDQ3OTEwOjMxNDE111==')
+  .expectStatus(401);
+
+});
