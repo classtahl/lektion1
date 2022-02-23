155eba3b3a5c1f76ac680f12a788173d0e5e7a26
Clas Ståhl <clas.stahl@edu.ecutbildning.se>
2022-02-23 11:40:22 +0100

    kodat om lite i pactum

--- pactum.test.js
+++ pactum.test.js
@@ -11,28 +11,12 @@ it('Get 401 status. Login with out credentials. Username and password is missing
     .expectResponseTime(20000);
 });
 
-it('Get 400-404 status with faulty user credentials, this means that login fails', async () => {
+it('Get 401 status without any login information, this means that login fails', async () => {
   pactum.spec()
     .get('https://handla.api.ica.se/api/login')
-    .expectStatus(401); // Server should return with answer 400-404 or similar without user credentials
+    .expectStatus(401); // Server should return with answer 401 without user credentials
 });
 
-it('Get 200 status with user credentials, this means that login works', async () => {
-   pactum.spec()
-  .get('https://handla.api.ica.se/api/login/')
-  .withHeaders('Authorization', 'Basic MTk3rrAyMDQ3OTEwOjMxNDE1OQ==')
-  .withHeaders({
-    'Content-Type': 'application/json'
-    .expectStatus(200);  // Server should return with answer faulty with user credentials
-  });
+});
 
-  it('Get 200 status with user credentials, this means that login works', async () => {
-  pactum.spec()
-  .get('https://handla.api.ica.se/api/login/')
-  .withHeaders('Authorization', 'Basic MTk3MzAyMDQ3OTEwOjMxNDE111==')
-  .withHeaders({
-    'Content-Type': 'application/json'
-  .expectStatus(401);
-
-});  
 
